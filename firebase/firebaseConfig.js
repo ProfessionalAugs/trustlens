bb6b6ea2320c557b0b0b1b31a77e93d20ac11a2f
Augs Professional <92197787+ProfessionalAugs@users.noreply.github.com>
2025-12-12 22:13:37 +0530

    Update firebaseConfig.js

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase
 // IMPORTANT: Replace these placeholder values with your actual Firebase project credentials
 // You can find these in your Firebase Console > Project Settings > General > Your apps
 const firebaseConfig = {
-   apiKey: "AIzaSyAUAcNc26zHQs987z8qxaeZOxZdIr4Ubl0",
-  authDomain: "trustlens-7b01b.firebaseapp.com",
-  projectId: "trustlens-7b01b",
-  storageBucket: "trustlens-7b01b.firebasestorage.app",
-  messagingSenderId: "639825344142",
-  appId: "1:639825344142:web:ce919b1a4d457aad30fbdf",
-  measurementId: "G-1EGCXYSBQ4"
+   apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
